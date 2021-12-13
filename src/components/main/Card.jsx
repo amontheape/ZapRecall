9bca1c586570f10da2a391949e78b3d7820b743c
amontheape <mcanedotome@gmail.com>
2021-12-13 15:05:50 -0300

    fix deploy

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import AnswerOptions from "./AnswerOptions"
 
 export default function Card({ data, status, setStatus, counter, setCounter, goal, setGoal, setTitle, setComponent, cardNumber}){
