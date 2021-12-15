d69628837a63958821ab5ab4790185f6065be628
Liaess <yann.melo@gmail.com>
2021-12-15 15:10:58 -0300

    Preparando revisão de código

--- src/components/main/Header.jsx
+++
@@ -1,9 +0,0 @@
-import minorLogo from '../../assets/img/minorLogo.png'
-
-export default function Header(){
-  return (
-    <header>
-      <img src={minorLogo} alt='smaller logo ZapRecall'/>
-    </header>
-  )
-}
